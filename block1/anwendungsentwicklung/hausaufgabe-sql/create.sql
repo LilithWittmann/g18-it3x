9ee51f7865083b29da3c249776e43ae28aaea9a2
Felix Wittmann <support@felicius12.de>
2013-05-22 09:19:37 +0200

    hausaufgaben fertig o0

--- block1/anwendungsentwicklung/hausaufgabe-sql/create.sql
+++ block1/anwendungsentwicklung/hausaufgabe-sql/create.sql
@@ -1,37 +1,36 @@
-drop database Moebelgeschaeft;
-create database Moebelgeschaeft;
-use Moebelgeschaeft;
+DROP DATABASE Moebelgeschaeft;
+CREATE database Moebelgeschaeft;
+USE Moebelgeschaeft;
 
 
 CREATE TABLE Bestellung (
-	BestNr int auto_increment primary key,
-	KNr int,
-	Datum date,
-	status bool
-
-	) ;
+	BestNr INT auto_increment PRIMARY KEY,
+	KNr INT,
+	Datum DATE,
+	status BOOL
+)  ENGINE = INNODB;
 
 
 create table Produkt(
-	ProNr int auto_increment primary key,
-	Bezeichnung varchar(70),
-	Bestand int
+	ProNr int auto_increment PRIMARY KEY,
+	Bezeichnung VARCHAR(70),
+	Bestand INT
 
-) ; 
+) ENGINE = INNODB; 
 
-create table Bestellposten
-(
-	ProNr int,
-	BestNr int,
-	Anzahl int
-	primary key(ProNr, BestNr),
-	foreign key(ProNr) references Produk(ProNr),
-	foreign key(BestNr) references Bestellung(BestNR)
-);
+CREATE TABLE Bestellposten(
+	ProNr INT NOT NULL,
+	BestNr INT NOT NULL,
+	Anzahl INT,
+	PRIMARY KEY (ProNr,BestNr),
+	FOREIGN KEY (ProNr) REFERENCES Produkt(ProNr),
+	FOREIGN KEY (BestNr) REFERENCES Bestellung(BestNr)
+) ENGINE=InnoDB;
 
-
-
-update table Produkt add column price double(10,2);
-
-update table Produkt price = price * 1.05;
-
+ALTER TABLE Produkt ADD COLUMN Preis DOUBLE(10,2);
+INSERT INTO Bestellung  (KNr,DATUM, Status) VALUES (1,DATE(NOW()),0);
+INSERT INTO Produkt (Bezeichnung, Bestand,Preis) VALUES ("Foo", 21, 9000);
+INSERT INTO Bestellposten (ProNr, BestNr, Anzahl) VALUES (1,1,42);
+UPDATE Produkt SET Preis = Preis * 1.05;
+DELETE FROM Bestellposten WHERE ProNr = 1;
+DELETE FROM Produkt WHERE ProNr = 1;
