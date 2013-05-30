0babaf0bd410669edda9cc9766e2fd91937021a7
Jan Thurau <jan@janthurau.de>
2013-05-30 09:35:25 +0200

    updates

--- projekt1/create.sql
+++ projekt1/create.sql
@@ -1,27 +1,4 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: 127.0.0.1 (MySQL 5.5.29-MariaDB-log)
-# Database: projekt-1
-# Generation Time: 2013-05-23 08:13:13 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table attribute_to_component
-# ------------------------------------------------------------
+SET FOREIGN_KEY_CHECKS = 0;
 
 CREATE TABLE `attribute_to_component` (
   `attribute_id` int(11) NOT NULL,
@@ -33,11 +10,6 @@ CREATE TABLE `attribute_to_component` (
   CONSTRAINT `fk_component_attributes_has_components_components1` FOREIGN KEY (`component_id`) REFERENCES `components` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table attribute_to_type
-# ------------------------------------------------------------
-
 CREATE TABLE `attribute_to_type` (
   `attribute_id` int(11) NOT NULL,
   `component_id` int(11) NOT NULL,
@@ -47,11 +19,6 @@ CREATE TABLE `attribute_to_type` (
   CONSTRAINT `fk_component_attributes_has_component_types_component_types1` FOREIGN KEY (`component_id`) REFERENCES `component_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table component_attributes
-# ------------------------------------------------------------
-
 CREATE TABLE `component_attributes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -59,21 +26,11 @@ CREATE TABLE `component_attributes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table component_ergonomics
-# ------------------------------------------------------------
-
 CREATE TABLE `component_ergonomics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table component_seals
-# ------------------------------------------------------------
 
 CREATE TABLE `component_seals` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -81,11 +38,6 @@ CREATE TABLE `component_seals` (
   `description` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table component_to_ergonomics
-# ------------------------------------------------------------
 
 CREATE TABLE `component_to_ergonomics` (
   `component_id` int(11) NOT NULL,
@@ -97,11 +49,6 @@ CREATE TABLE `component_to_ergonomics` (
   CONSTRAINT `fk_components_has_component_ergonomics_component_ergonomics1` FOREIGN KEY (`attribute_id`) REFERENCES `component_ergonomics` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table component_to_seals
-# ------------------------------------------------------------
-
 CREATE TABLE `component_to_seals` (
   `component_id` int(11) NOT NULL,
   `seal_id` int(11) NOT NULL,
@@ -111,21 +58,11 @@ CREATE TABLE `component_to_seals` (
   CONSTRAINT `fk_components_has_component_seals_component_seals1` FOREIGN KEY (`seal_id`) REFERENCES `component_seals` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table component_types
-# ------------------------------------------------------------
-
 CREATE TABLE `component_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table components
-# ------------------------------------------------------------
 
 CREATE TABLE `components` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -136,12 +73,6 @@ CREATE TABLE `components` (
   KEY `fk_components_component_types1` (`type_id`),
   CONSTRAINT `fk_components_component_types1` FOREIGN KEY (`type_id`) REFERENCES `component_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table type_to_attribute
-# ------------------------------------------------------------
-
 CREATE TABLE `type_to_attribute` (
   `type_id` int(11) NOT NULL,
   `attribute_id` int(11) NOT NULL,
@@ -152,11 +83,4 @@ CREATE TABLE `type_to_attribute` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
